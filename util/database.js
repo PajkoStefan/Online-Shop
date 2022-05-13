0e49baa72342ac5b6236646622e9108b0a4a9c2e
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-05-13 02:04:24 +0200

    sequelize added

--- util/database.js
+++ util/database.js
@@ -1,13 +1,10 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const pool = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USERNAME,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD
-});
+const sequelize = new Sequelize(
+    process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+        dialect: 'mysql', host: process.env.HOST
+    });
 
-
-module.exports = pool.promise();
+    module.exports = sequelize;
