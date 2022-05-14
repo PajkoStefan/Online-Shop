7979a75dd7acb7bd0b357c02ebee4e4111db135f
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-05-14 21:34:17 +0200

    add Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -77,15 +77,42 @@ exports.getCart = (req, res, next) => {
     .catch((err) => {
       console.log(err);
     });
-
 };
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  Product.findById(productId, (product) => {
-    Cart.addProduct(productId, product.price);
-  });
-  res.redirect("/cart");
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: productId } });
+    })
+    .then((cartProducts) => {
+      let product;
+      if (cartProducts.length > 0) {
+        product = cartProducts[0];
+      }
+
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(productId);
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
