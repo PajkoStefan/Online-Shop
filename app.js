b7bca25771e89c933ea2d5946898b21737801fcb
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-05-14 23:03:06 +0200

    Order model added

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 // controllers
 const errorController = require("./controllers/error");
@@ -59,8 +61,13 @@ User.hasMany(Product);
 
 User.hasOne(Cart);
 Cart.belongsTo(User);
+
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
 
 //sequelize
 sequelize
