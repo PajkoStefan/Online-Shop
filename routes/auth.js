4f560ca7069c8f6ca201363df71243a5edcfa5be
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-05-29 20:39:52 +0200

    Sanitizing Data

--- routes/auth.js
+++ routes/auth.js
@@ -23,10 +23,14 @@ router.get("/new-password", authController.getNewPassword);
 router.post(
   "/login",
   [
-    body("email").isEmail().withMessage("Please, enter a valid email."),
+    body("email")
+      .isEmail()
+      .withMessage("Please, enter a valid email.")
+      .normalizeEmail(),
     body("password", "Password has to be valid.")
       .isLength({ min: 5 })
-      .isAlphanumeric(),
+      .isAlphanumeric()
+      .trim(),
   ],
   authController.postLogin
 );
@@ -49,19 +53,22 @@ router.post(
             );
           }
         });
-      }),
+      })
+      .normalizeEmail(),
     body(
       "password",
       "Please enter a password with only numbers and text and at least 5 characters."
     )
       .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body("confirmPassword").custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Passwords have to match!");
-      }
-      return true;
-    }),
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword").trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords have to match!");
+        }
+        return true;
+      }),
   ],
   authController.postSignUp
 );
